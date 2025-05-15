18e10bdb3ed7c9d4f18c64d75e46a0a087471515
Lopeszs <isabelafl07005@gmail.com>
2025-05-15 09:35:50 -0400

    add arquivos

---
+++ main.c
@@ -0,0 +1,8 @@
+/**************************************************
+*
+* Nome dos(as) estudantes: Isabela Fernandes Lopes e Maria Eduarda da Silva Gonçalves
+* Trabalho 1
+* Disciplina: Estrutura de Dados
+* Objetivo:
+*/
+#include <stdio.h>
